b4ad3df0d646e1fd352eaf69ad499754e8a486aa
Kayu Lui <klui69308@gmail.com>
2020-12-09 11:22:04 -0600

    first commit

---
+++ StringArrayExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringArrayExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your string: ");
+            string input = Console.ReadLine();
+            char[] inputCharactor = input.ToCharArray();
+
+            for (int i = inputCharactor.Length - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    Console.Write(inputCharactor[i] + " ");
+                }
+                else
+                {
+                    Console.Write(inputCharactor[i] + ", ");
+                }
+            }
+        }
+    }
+}
